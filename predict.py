0bbccda128635f42e7362f66d1370be6665d9e8c
LibraTang <libratang@qq.com>
2022-04-26 21:20:37 +0800

    [Modify]Improve the process of prediction

--- predict.py
+++ predict.py
@@ -1,3 +1,5 @@
+import time
+
 import joblib
 import numpy as np
 import fft_process
@@ -7,52 +9,60 @@ import shared_variable
 from sklearn.preprocessing import normalize
 
 
+# load trained model
+Val_R = joblib.load("C:/Users/Libra/OneDrive - std.uestc.edu.cn/UR/BCI/model/DEAP_Emotion/val_model.pkl")
+Aro_R = joblib.load("C:/Users/Libra/OneDrive - std.uestc.edu.cn/UR/BCI/model/DEAP_Emotion/aro_model.pkl")
+Dom_R = joblib.load("C:/Users/Libra/OneDrive - std.uestc.edu.cn/UR/BCI/model/DEAP_Emotion/dom_model.pkl")
+Lik_R = joblib.load("C:/Users/Libra/OneDrive - std.uestc.edu.cn/UR/BCI/model/DEAP_Emotion/lik_model.pkl")
+
+
 # 线程1，实时收集EEG数据
 def thread_collect_data():
-    print('thread %s is running...' % threading.current_thread().name)
+    print('Thread %s is running...' % threading.current_thread().name)
     while True:
         get_data.get_data()
 
-def predict(data_test, model):
-    """
-    arguments:  data_test: testing dataset
-                model: scikit-learn model
 
-    return:     float
-    """
-    output = model.predict(data_test)
-
+def predict(data, model):
+    output = model.predict(data)
     return np.mean(output)
 
 
-print('thread %s is running...' % threading.current_thread().name)
+print('Thread %s is running...' % threading.current_thread().name)
 # 启动收集EEG线程
-t = threading.Thread(target=thread_collect_data(), name='CollectThread')
+t = threading.Thread(target=thread_collect_data, name='CollectThread')
 t.start()
 
-# 轮询共享数组
-while shared_variable.data.size == 0:
-    continue
+while True:
+    # 轮询共享数组
+    while len(shared_variable.data) == 0:
+        print("EEG data is empty...")
+        time.sleep(10)
+        continue
 
-# 取出收集的EEG
-raw_data = shared_variable.data[0]
+    print("Get latest EEG data...")
+    # 取出最新收集的EEG并清空共享数组
+    raw_data = shared_variable.data[-1]
+    shared_variable.data.clear()
 
-# fft处理
-fft_data = fft_process.fft_process(raw_data)
-data_test = normalize(fft_data)
+    # fft处理
+    fft_data = fft_process.fft_process(raw_data)
+    fft_data = normalize(fft_data)
 
-# load trained model
-Val_R = joblib.load("model/val_model.pkl")
-Aro_R = joblib.load("model/aro_model.pkl")
-Dom_R = joblib.load("model/dom_model.pkl")
-Lik_R = joblib.load("model/lik_model.pkl")
+    # start prediction
+    score_valence = predict(fft_data, Val_R)
+    score_arousal = predict(fft_data, Aro_R)
+
+    print("Valence: %f" % score_valence)
+    print("Arousal: %f" % score_arousal)
+
+    if score_valence >= 5 and score_arousal >= 5:
+        print("Current emotion prediction: Happy/Excited")
+    elif score_valence <= 5 and score_arousal >= 5:
+        print("Current emotion prediction: Angry/Frustrated")
+    elif score_valence <= 5 and score_arousal <= 5:
+        print("Current emotion prediction: Depressed/Tired")
+    else:
+        print("Current emotion prediction: Relaxed/Calm")
 
 
-# start prediction
-print("Valence: %f" % predict(data_test, Val_R))
-
-print("Arousal: %f" % predict(data_test, Aro_R))
-
-print("Domain: %f" % predict(data_test, Dom_R))
-
-print("Like: %f" % predict(data_test, Lik_R))
--- shared_variable.py
+++ shared_variable.py
@@ -1,4 +1,2 @@
-import numpy as np
-
-# 共享脑电波数组，大小为1
-data = np.empty([1, 1])
+# 共享脑电波数组
+data = []
