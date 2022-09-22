5b8935c0b143cf9a5adf2959bee42d5405df4f55
LibraTang <1026960839@qq.com>
2022-09-22 17:14:55 +0800

    [Add] Music-Emotion analyse

--- predict.py
+++ predict.py
@@ -1,10 +1,14 @@
 import joblib
 import numpy as np
 import mne
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import normalize
 from fft_process import fft_process
 from mne.io import read_raw_edf
+
+tempo_sequence = [4, 5, 1]  # 按节奏重排文件顺序
+tone_sequence = [2, 6, 3]  # 按音调重排文件顺序
 
 def predict(data, model):
     output = model.predict(data)
@@ -17,32 +21,94 @@ Aro_R = joblib.load("C:/Users/Libra/OneDrive - std.uestc.edu.cn/UR/BCI/model/DEA
 Dom_R = joblib.load("C:/Users/Libra/OneDrive - std.uestc.edu.cn/UR/BCI/model/DEAP_Emotion/dom_model.pkl")
 Lik_R = joblib.load("C:/Users/Libra/OneDrive - std.uestc.edu.cn/UR/BCI/model/DEAP_Emotion/lik_model.pkl")
 
-raw = read_raw_edf("music_emotion_eeg/S01E01_filtered.edf", preload=False)
 
-events_from_annot, event_dict = mne.events_from_annotations(raw)
+tempo_x = [90, 120, 150]  # tempo x轴
+tone_x = ['C', 'E', 'G']  # tone x轴
 
-start = events_from_annot[0][0]
-end = events_from_annot[1][0]
+# 划分子图
+fig, axes = plt.subplots(2, 2)
+valence_tempo_ax = axes[0, 0]
+valence_tone_ax = axes[0, 1]
+arousal_tempo_ax = axes[1, 0]
+arousal_tone_ax = axes[1, 1]
 
-rawEEG = raw.get_data()  # 读取原始信息
-music_eeg = rawEEG[[0, 1, 5, 6, 10, 11, 14, 15], start:end+1]  # 获取听音乐时段的eeg
+fig.set_size_inches(16, 9)
 
-fft_data = fft_process(music_eeg)
-fft_data = normalize(fft_data)
+valence_tempo_ax.set_xlabel('tempo(bpm)')
+valence_tempo_ax.set_ylabel('valence')
+valence_tone_ax.set_xlabel('tone')
+valence_tone_ax.set_ylabel('valence')
+arousal_tempo_ax.set_xlabel('tempo(bpm)')
+arousal_tempo_ax.set_ylabel('arousal')
+arousal_tone_ax.set_xlabel('tone')
+arousal_tone_ax.set_ylabel('arousal')
 
-score_valence = predict(fft_data, Val_R)
-score_arousal = predict(fft_data, Aro_R)
+# 读取所有受试者的数据
+for i in range(1, 19):
+    # 音乐2的数据需要跳过受试者10、11、17
+    if i in [10, 11, 17]:
+        continue
 
-print("Valence: %f" % score_valence)
-print("Arousal: %f" % score_arousal)
+    # 记录valence和arousal的数值
+    valence_tempo_arr = []
+    arousal_tempo_arr = []
+    valence_tone_arr = []
+    arousal_tone_arr = []
 
-if 4.5 <= score_valence <= 5.5 and 4.5 <= score_arousal <= 5.5:
-    print("Emotion prediction: Calm")
-elif score_valence >= 5 and score_arousal >= 5:
-    print("Emotion prediction: Happy/Excited")
-elif score_valence <= 5 and score_arousal >= 5:
-    print("Emotion prediction: Angry/Frustrated")
-elif score_valence <= 5 and score_arousal <= 5:
-    print("Emotion prediction: Depressed/Tired")
-elif score_valence >= 5 and score_arousal <= 5:
-    print("Emotion prediction: Relaxed/Calm")
+    # 读取节奏相关的eeg数据
+    for j in tempo_sequence:
+        eeg_path = f'music_emotion_eeg/music2/S{i:02d}/S{i:02d}E{j:02d}_filtered.edf'
+        raw = read_raw_edf(eeg_path, preload=False)
+
+        events_from_annot, event_dict = mne.events_from_annotations(raw)
+
+        start = events_from_annot[0][0]
+        end = events_from_annot[1][0]
+
+        rawEEG = raw.get_data()  # 读取原始信息
+        music_eeg = rawEEG[[0, 1, 5, 6, 10, 11, 14, 15], start:end+1]  # 获取指定通道听音乐时段的eeg
+
+        fft_data = fft_process(music_eeg)
+        fft_data = normalize(fft_data)
+
+        score_valence = predict(fft_data, Val_R)
+        score_arousal = predict(fft_data, Aro_R)
+
+        valence_tempo_arr.append(score_valence)
+        arousal_tempo_arr.append(score_arousal)
+
+        print("Valence: %f" % score_valence)
+        print("Arousal: %f" % score_arousal)
+
+    valence_tempo_ax.plot(tempo_x, valence_tempo_arr)
+    arousal_tempo_ax.plot(tempo_x, arousal_tempo_arr)
+
+    # 读取音调相关的eeg数据
+    for k in tone_sequence:
+        eeg_path = f'music_emotion_eeg/music2/S{i:02d}/S{i:02d}E{k:02d}_filtered.edf'
+        raw = read_raw_edf(eeg_path, preload=False)
+
+        events_from_annot, event_dict = mne.events_from_annotations(raw)
+
+        start = events_from_annot[0][0]
+        end = events_from_annot[1][0]
+
+        rawEEG = raw.get_data()  # 读取原始信息
+        music_eeg = rawEEG[[0, 1, 5, 6, 10, 11, 14, 15], start:end+1]  # 获取指定通道听音乐时段的eeg
+
+        fft_data = fft_process(music_eeg)
+        fft_data = normalize(fft_data)
+
+        score_valence = predict(fft_data, Val_R)
+        score_arousal = predict(fft_data, Aro_R)
+
+        valence_tone_arr.append(score_valence)
+        arousal_tone_arr.append(score_arousal)
+
+        print("Valence: %f" % score_valence)
+        print("Arousal: %f" % score_arousal)
+
+    valence_tone_ax.plot(tone_x, valence_tone_arr)
+    arousal_tone_ax.plot(tone_x, arousal_tone_arr)
+
+plt.show()
